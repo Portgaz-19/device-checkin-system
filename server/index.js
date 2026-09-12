ffc07ba25cca85a7f1e83c6e72f9b15f3315e38d
Eazy 1.0 <effeyotahisrael@gmail.com>
2026-09-12 16:05:58 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -20,18 +20,6 @@ const URI = process.env.MONGO_URI;
 const app = express();
 
 
-
-const allowedOrigins = process.env.FRONTEND_URL
-  ? [process.env.FRONTEND_URL, "http://localhost:5173/"]
-  : ["http://localhost:5173/"];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  }),
-);
-
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
